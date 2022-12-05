0e1c4dc64a3d6f7210415615e82d8d99ac33c9da
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-04 23:32:07 -0300

    fix: added httpclient source to versions.tf

--- versions.tf
+++ versions.tf
@@ -4,5 +4,9 @@ terraform {
       source = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
+    httpclient = {
+      source  = "dmachard/http-client"
+      version = ">= 0.0.3"
+    }
   }
 }
